2545b29f08c0f9062c52ff3ed2ad9da65fe63f8f
Script Kitty <hakuna.matata.kitty@gmail.com>
2017-03-13 20:51:01 -0500

    Committing right before I make a bunch of dumb crazy mistakes

---
+++ ReQuit/ConversationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ConversationViewController.swift
+//  ReQuit
+//
+//  Created by Winston Van on 3/13/17.
+//  Copyright © 2017 Winston Van. All rights reserved.
+//
+
+import UIKit
+
+
+class ConversationViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    private let cellId = "cellId"
+    
+}
+
+
+class ChatMessageCell: BaseCell {
+    
+}
