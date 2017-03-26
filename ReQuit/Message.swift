f50a2c7ae6bcbaf3b639f843113838730b993547
Script Kitty <hakuna.matata.kitty@gmail.com>
2017-03-25 23:07:02 -0500

    Fixed annoying time disordering bug.

--- ReQuit/Message.swift
+++ ReQuit/Message.swift
@@ -11,11 +11,11 @@ import UIKit
 struct Message {
     let sender: String // If nil, anon
     let message: String
-    let time: Int
+    let time: Double
     
     init (messageDetails: NSDictionary?) {
         self.sender = messageDetails?["sender"] as? String ?? "Anon" // If nil, anon
         self.message = messageDetails?["message"] as? String ?? "Message not found"
-        self.time = messageDetails?["time"] as? Int ?? 0
+        self.time = (messageDetails?["time"] as? Double)!
     }
 }
